ff0050b2543e0a86d51d74817b470ebd9616c04e
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-09 21:12:19 +0900

    Mapper ifs 생성

---
+++ src/main/java/com/example/springboard/mapper/BoardMapper.java
@@ -0,0 +1,22 @@
+package com.example.springboard.mapper;
+
+import com.example.springboard.domain.BoardDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    //저장
+    public int insertBoard(BoardDTO boardDTO);
+    //세부사항
+    public BoardDTO selectBoardDetail(Long idx);
+    //수정
+    public int updateBoard(BoardDTO boardDTO);
+    //삭제
+    public int deleteBoard(Long idx);
+    //게시판 전체조회
+    public List<BoardDTO> selectBoardList();
+    //게시판 갯수
+    public int selectBoardTotalCount();
+}
