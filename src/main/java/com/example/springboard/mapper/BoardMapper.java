9f42c698e1627963730907aa61da19f757c18755
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-10 14:42:20 +0900

    리턴 타입 파라미터 타입 변경

--- src/main/java/com/example/springboard/mapper/BoardMapper.java
+++ src/main/java/com/example/springboard/mapper/BoardMapper.java
@@ -1,6 +1,6 @@
 package com.example.springboard.mapper;
 
-import com.example.springboard.domain.BoardDTO;
+import com.example.springboard.domain.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,15 +8,15 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
     //저장
-    public int insertBoard(BoardDTO boardDTO);
+    public int insertBoard(Board board);
     //세부사항
-    public BoardDTO selectBoardDetail(Long idx);
+    public Board selectBoardDetail(Long idx);
     //수정
-    public void updateBoard(BoardDTO boardDTO);
+    public void updateBoard(Board board);
     //삭제
     public void deleteBoard(Long idx);
     //게시판 전체조회
-    public List<BoardDTO> selectBoardList();
+    public List<Board> selectBoardList();
     //게시판 갯수
     public int selectBoardTotalCount();
 }
