1673b9c2e095a7dfe14ad546697dd5b4a9ce5cc6
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-10 14:43:53 +0900

    게시물 수정을 controller로부터 위임받음

--- src/main/java/com/example/springboard/service/BoardService.java
+++ src/main/java/com/example/springboard/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.example.springboard.service;
 
-import com.example.springboard.domain.BoardDTO;
+import com.example.springboard.controller.dto.BoardForm;
+import com.example.springboard.domain.Board;
 import com.example.springboard.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,21 +19,54 @@ public class BoardService {
     /**
      * 게시판 등록
      */
-    public void registerBoard(BoardDTO boardDTO) {
-        if( boardDTO.getIdx() == null ) {
+    public void registerBoard(Board board) {
+        if( board.getIdx() == null ) {
             //생성
-            boardMapper.insertBoard(boardDTO);
+            boardMapper.insertBoard(board);
         }else {
             //수정
-            boardMapper.updateBoard(boardDTO);
+            boardMapper.updateBoard(board);
+        }
+    }
+
+    /**
+     * 게시판 목록 조회
+     */
+    public List<Board> getBoardList() {
+        int totalCnt = boardMapper.selectBoardTotalCount();
+
+        if( totalCnt > 0 ) {
+            return boardMapper.selectBoardList();
+        }else {
+            //TODO DB에 데이터가 하나도 없을 때 처리하는 로직 추가 예정
+            throw new IllegalStateException();
         }
     }
 
     /**
      * 게시판 상세정보
      */
-    public BoardDTO getBoardDetail(Long idx) {
+    public Board getBoardDetail(Long idx) {
         return boardMapper.selectBoardDetail(idx);
+    }
+
+    /**
+     * 게시판 내용 수정
+     * idx로 영속성 데이터를 추출 후, 영속성 데이터에 update
+     */
+    public void updateBoard(BoardForm form) {
+        //영속성 데이터 추출
+        Board findBoard = boardMapper.selectBoardDetail(form.getIdx());
+        log.info("영속성 데이터 : {} ", findBoard);
+
+        //영속성 데이터에 저장
+        findBoard.setNoticeYn(form.getNoticeYn());
+        findBoard.setSecretYn(form.getSecretYn());
+        findBoard.setTitle(form.getTitle());
+        findBoard.setWriter(form.getWriter());
+        findBoard.setTitle(form.getTitle());
+
+        boardMapper.updateBoard(findBoard);
     }
 
     /**
@@ -40,25 +74,5 @@ public class BoardService {
      */
     public void deleteBoard(Long idx) {
         boardMapper.deleteBoard(idx);
-    }
-
-    /**
-     * 게시판 목록 조회
-     */
-    public List<BoardDTO> getBoardList() {
-        int totalCnt = boardMapper.selectBoardTotalCount();
-
-        if( totalCnt > 0 ) {
-            return boardMapper.selectBoardList();
-        }else {
-            throw new IllegalStateException();
-        }
-    }
-
-    /**
-     * 게시판 내용 수정
-     */
-    public void updateBoard(BoardDTO boardDTO) {
-        boardMapper.updateBoard(boardDTO);
     }
 }
